312f396a689012132866a16792fb6e00e6aeb2fb
Miku Watanabe <mikuwatanabe@mac.lan>
2026-06-01 14:13:37 -0400

    feat: add demo login

--- app/actions/login-action.ts
+++ app/actions/login-action.ts
@@ -3,6 +3,7 @@
 import { z } from "zod";
 import { createClient } from "@/lib/supabase/server";
 import { redirect } from "next/navigation";
+import { getUser } from "./diary/read-action";
 
 const LoginSchema = z.object({
   email: z.string().email("Please enter a valid email address"),
@@ -36,6 +37,46 @@ export async function login(initialState: any, formData: FormData) {
     return {
       success: false,
       message: "Failed to login. Please try again.",
+      errors: {},
+    };
+  }
+
+  redirect("/diary");
+}
+
+export async function loginWithDemo() {
+  const supabase = await createClient();
+  const { error: signInError } = await supabase.auth.signInWithPassword({
+    email: "demo@example.com",
+    password: process.env.DEMO_USER_PASSWORD!,
+  });
+  if (signInError) {
+    return {
+      success: false,
+      message: "Failed to login with demo account. Please try again.",
+      errors: {},
+    };
+  }
+  const userResult = await getUser();
+  if (!userResult.success || !userResult.userData?.id) {
+    return {
+      success: false,
+      message: "Failed to login with demo account. Please try again.",
+      errors: {},
+    };
+  }
+
+  // delete all the demo data from the database except seed data
+  const { error: deleteError } = await supabase
+    .from("diaries")
+    .delete()
+    .eq("user_id", userResult.userData.id)
+    .or("is_sample.eq.false");
+
+  if (deleteError) {
+    return {
+      success: false,
+      message: "Failed to login with demo account. Please try again.",
       errors: {},
     };
   }
